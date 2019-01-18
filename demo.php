fad9d283870b77ca92fd7eccf432904889d6f3ce
vrushalibhosale <vrushali.bhosale@qed42.com>
2019-01-18 16:09:28 +0530

    simple form handling

--- demo.php
+++ demo.php
@@ -23,5 +23,35 @@ $obj=new Account();
 $obj->isThere("meena");
 ?>
 
+<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+  Name: <input type="text" name="fname"><br><br>
+  Email:<input type="text" name="email"><br><br>
+  <input type="submit"><br><br>
+</form>
+
+
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+  $name= $_REQUEST['fname'];
+  $email=$_REQUEST['email'];
+  if(empty($name))
+  {
+      echo "name is empty"."<br>";
+  }
+  if(empty($email))
+  {
+    echo "email is empty";
+  }
+
+echo "<h2>Inputs:</h2>";
+echo $name."<br>";
+echo $email;
+}
+
+?>
+
 </body>
 </html>
